6e9c0bbc30c50b0104debcdcfcf51410737ae00b
Thomas Nelson <thnlsn@gmail.com>
2021-04-02 13:23:57 -0700

    created blank components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './css/style.css';
 
 function App() {
-  return <div className='App'>▓▓▓▓▓Test▓▓▓▓▓</div>;
+  return (
+    <div className='App'>
+      <Route path='/' exact component={ExercisesList} />
+      <Route path='/edit/:id' component={EditExercise} />
+      <Route path='/create' component={CreateExercise} />
+      <Route path='/user' component={CreateUser} />
+    </div>
+  );
 }
 
 export default App;
