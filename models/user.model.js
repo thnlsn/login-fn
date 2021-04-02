52461f8099510780cc809d39d1669eb19823c42f
Thomas Nelson <thnlsn@gmail.com>
2021-04-01 20:35:07 -0700

    post request working correctly for user adding

---
+++ models/user.model.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      minlength: 3,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
+
+/* const mongoose = require('mongoose');
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  stars: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model('user', UserSchema); // ('modelname', Schema) */
--- server.js
+++ server.js
@@ -1,4 +1,40 @@
 const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// Connecting the MongoDB Database
+const URI = process.env.ATLAS_URI; // Private URI to connect to MongoDB Atlas
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully');
+});
+
+// Telling server to use the route files
+const exercisesRouter = require('./routes/exercises'); // import routes
+const usersRouter = require('./routes/users'); // import routes
+
+app.use('/exercises', exercisesRouter); // middleware to handle route /excercises
+app.use('/users', usersRouter); // middleware to handle route /users
+
+// Listening on PORT
+app.listen(PORT, () => {
+  console.log(`Server is running in ${process.env.NODE_ENV} on port: ${PORT}`);
+});
+
+/* const express = require('express');
 connectDB = require('./config/db');
 const app = express();
 const path = require('path');
@@ -11,7 +47,7 @@ app.use(express.json({ extended: false }));
 
 app.use(express.static('public'));
 // GET home route
-app.get('/login-fn', (_, res) => {
+app.get('/login-fn/users', (_, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
@@ -22,4 +58,4 @@ const PORT = process.env.PORT || 3000;
 // Start server with PORT
 app.listen(PORT, () =>
   console.log(`App is running in ${process.env.NODE_ENV} on port: ${PORT}`)
-);
+); */
