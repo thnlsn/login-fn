d8fac33d0bfde236a928ea8fb6ad0d486c2ff181
Thomas Nelson <thnlsn@gmail.com>
2021-04-05 16:33:50 -0700

    refactored db connection to use async await syntax over promise .then

--- config/db.js
+++ config/db.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
+const URI = process.env.ATLAS_URI; // Private URI to connect to MongoDB Atlas
 
 const connectDB = async () => {
-  // Connecting the MongoDB Database
-  const URI = process.env.ATLAS_URI; // Private URI to connect to MongoDB Atlas
-  mongoose.connect(URI, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  });
+  try {
+    // Connecting the MongoDB Database
+    await mongoose.connect(URI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB database connection established successfully.');
+  } catch (err) {
+    console.error(`MongoDB Connection Error: ${err.message}`);
+    process.exit(1); // 1 is uncaught exception status code
+  }
+
   const connection = mongoose.connection;
-  connection.once('open', () => {
-    console.log('MongoDB database connection established successfully');
-  });
 };
 
 module.exports = connectDB;
