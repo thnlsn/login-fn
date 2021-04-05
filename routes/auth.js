515c6a339dff95ffad83c18de5fe69b578ee283e
Thomas Nelson <thnlsn@gmail.com>
2021-04-05 16:21:07 -0700

    fixed comments and request type on auth route

--- routes/auth.js
+++ routes/auth.js
@@ -3,15 +3,17 @@ const router = require('express').Router();
 // @route     GET /auth/
 // @desc      Read/get the data pertaining to the currently logged in user
 // @access    PRIVATE (requires token)
-router.get('/', (req, res) => {
+router.get('/', (_, res) => {
   res.send('Read/get the data pertaining to the currently logged in user');
 });
 
 // @route     POST /auth/
-// @desc      Post/submit form data to then authenticate the user and log them in, while also generating a token
-// @access    PUBLIC (because everyone is allowed to try to log in, it is once a token is recieved that routes will then be private to that specific user)
-router.get('/', (req, res) => {
-  res.send('Read/get the data pertaining to the currently logged in user');
+// @desc      Post/submit form data to then authenticate the user.
+// @access    PUBLIC (because everyone is allowed to try to log in)
+router.post('/', (req, res) => {
+  res.send(
+    'Post/submit form data to then authenticate the user and log them in, while also generating a token'
+  );
 });
 
 module.exports = router;
