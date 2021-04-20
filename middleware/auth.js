ec5d345ee6a69f1248fe410f4def4f3a5bc6d00a
Thomas Nelson <thnlsn@gmail.com>
2021-04-19 23:32:42 -0700

    created function for auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,8 @@
 // Middleware is a function that has access to the req/res cycle
 const jwt = require('jsonwebtoken');
 const config = require('config');
+
+module.exports = function (req, res, next) {
+  // Get token from header
+  const token = req.header('x-auth-token');
+};
